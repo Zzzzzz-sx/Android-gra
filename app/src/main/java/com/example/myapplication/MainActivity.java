6bdc86e099e624a17fc27e2e40e73fc33bea8716
Zzzz <1362976224@qq.com>
2024-04-17 09:26:50 +0800

    主页点击身份跳转登录

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,18 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button patlog,doclog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        patlog = findViewById(R.id.doc_to_login);
+        doclog = findViewById(R.id.pat_to_login);
+        patlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Login_doc.class));
+            }
+        });
+        doclog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Login_pat.class));
+            }
+        });
     }
-    //    跳转到医生登录页面
-    public void docterlogin(View view) {
-        startActivity(new Intent(this,Login_doc.class));
-    }
-
-
 }
