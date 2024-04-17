f6588cb2d762ef4eca9dcc2d2454fd8ca4e90ed1
Zzzz <1362976224@qq.com>
2024-04-17 13:59:19 +0800

    在患者个人资料页面读取数据库展示出已有的信息

--- app/src/main/java/com/example/myapplication/Pat_Personalinfo.java
+++ app/src/main/java/com/example/myapplication/Pat_Personalinfo.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.PersistableBundle;
@@ -18,7 +19,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Pat_Personalinfo extends AppCompatActivity {
     EditText inputname,inputid,inputphonenum;
     Button saveinfo;
-    String patname,patpersonalid,patphonenum;
+    String setpatname,setpatpersonalid,setpatphonenum,getpatname,getpatpersonalid,getpatphonenum;
     String loginname;
     private PatSQliteOpenHelper dbHelper;
     @SuppressLint("MissingInflatedId")
@@ -36,16 +37,15 @@ public class Pat_Personalinfo extends AppCompatActivity {
             loginname = intent.getStringExtra("login_name");
             Log.d("Pat_Personalinfo","登录用户名："+loginname);
         }
-
+        getinfo();
         saveinfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int idlength = inputid.getText().length();
                 int phonelength = inputphonenum.getText().length();
-                patname = inputname.getText().toString();
-                patpersonalid = inputid.getText().toString();
-                patphonenum = inputphonenum.getText().toString();
-
+                setpatname = inputname.getText().toString();
+                setpatpersonalid = inputid.getText().toString();
+                setpatphonenum = inputphonenum.getText().toString();
                 Log.d("Pat_Personalinfo","长度为："+inputid.getText().length());
                 if(idlength!=18){
                     Toast.makeText(view.getContext(),"身份证填写有误，请重新填写！",Toast.LENGTH_SHORT).show();
@@ -53,7 +53,7 @@ public class Pat_Personalinfo extends AppCompatActivity {
                 else if (phonelength!=11){
                     Toast.makeText(view.getContext(),"手机号填写有误，请重新填写！",Toast.LENGTH_SHORT).show();
                 }
-                else if (patname==null){
+                else if (setpatname==null){
                     Toast.makeText(view.getContext(),"姓名填写有误，请重新填写！",Toast.LENGTH_SHORT).show();
                 }else {
                     save();
@@ -64,14 +64,32 @@ public class Pat_Personalinfo extends AppCompatActivity {
         });
     }
 
+    private void getinfo() {
+        dbHelper = new PatSQliteOpenHelper(this,"Patinfo.db",null,1);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Cursor cursor = db.query("patient",null,"pat_login_name=?",new String[]{loginname},null,null,null);
+        if (cursor.moveToFirst()){
+            do {
+                getpatname = cursor.getString(cursor.getColumnIndexOrThrow("pat_name"));
+                getpatpersonalid = cursor.getString(cursor.getColumnIndexOrThrow("pat_personalid"));
+                Log.d("Pat_Personalinfo","身份证"+getpatpersonalid);
+                getpatphonenum = cursor.getString(cursor.getColumnIndexOrThrow("pat_phonenum"));
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        inputname.setText(getpatname);
+        inputid.setText(getpatpersonalid);
+        inputphonenum.setText(getpatphonenum);
+    }
+
     private void save() {
         //读取数据库
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues Patinfo = new ContentValues();
         //设置字段
-        Patinfo.put("pat_name",patname);
-        Patinfo.put("pat_personalid",patpersonalid);
-        Patinfo.put("pat_phonenum",patphonenum);
+        Patinfo.put("pat_name",setpatname);
+        Patinfo.put("pat_personalid",setpatpersonalid);
+        Patinfo.put("pat_phonenum",setpatphonenum);
         //插入数据库中
         db.update("patient",Patinfo,"pat_login_name=?",new String[]{loginname});
         Toast.makeText(this,"保存成功！",Toast.LENGTH_SHORT).show();
