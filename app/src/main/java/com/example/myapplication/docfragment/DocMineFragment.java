9c249a0ad5f06865f2e42bf56ea3fc2d7cc96a22
Zzzz <1362976224@qq.com>
2024-04-18 10:52:15 +0800

    新增工作量统计的xml文件，添加点击跳转工作量统计页面的事件

--- app/src/main/java/com/example/myapplication/docfragment/DocMineFragment.java
+++ app/src/main/java/com/example/myapplication/docfragment/DocMineFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.docfragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -10,13 +11,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.myapplication.Doc_workcount;
 import com.example.myapplication.R;
 
 //医生端个人信息界面
 public class DocMineFragment extends DocBaseFragment {
 //    ImageView docimg;
-    TextView showname;
+    TextView showname,workcount;
     String name;
+
 
 //    public static DocMineFragment newInstance(String logname) {
 //        DocMineFragment fragment = new DocMineFragment();
@@ -44,10 +47,17 @@ public class DocMineFragment extends DocBaseFragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         showname = getActivity().findViewById(R.id.tv_mine_name);
+        workcount = getActivity().findViewById(R.id.tv_work_statistics);
         Bundle bundle = getArguments();
         name = bundle.getString("name");
         Log.d("DocMineFragment","name="+name);
         showname.setText(name);
+        workcount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), Doc_workcount.class));
+            }
+        });
 //        Showname();
 //        Intent intent = Intent.getIntent();
 //        if(intent!= null){
