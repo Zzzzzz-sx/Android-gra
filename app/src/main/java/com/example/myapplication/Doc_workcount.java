9864f35fc811cba33a818d7de61616bb58700a9a
Zzzz <1362976224@qq.com>
2024-04-22 13:54:17 +0800

    工作量统计：根据特定项目进行筛选（未实现全选）

--- app/src/main/java/com/example/myapplication/Doc_workcount.java
+++ app/src/main/java/com/example/myapplication/Doc_workcount.java
@@ -9,9 +9,12 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,10 +22,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.myapplication.SQliteOpenHelper.MySQliteOpenHelper;
 
 public class Doc_workcount extends AppCompatActivity {
-    Integer charge,chargesum;
+    Integer charge,chargesum,itemindex;
     TextView showstarttime,showendtime,showworkcount;
+    Spinner showitemname;
     Button selectstarttime,selectendtime,getworkcount;
-    String getstarttime,getendtime,nameget;
+    String getstarttime,getendtime,nameget,getitemname;
     private MySQliteOpenHelper dbHelper;
     @SuppressLint("MissingInflatedId")
     @Override
@@ -38,6 +42,7 @@ public class Doc_workcount extends AppCompatActivity {
         selectstarttime = findViewById(R.id.btn_select_startdate);
         selectendtime = findViewById(R.id.btn_select_enddate);
         getworkcount = findViewById(R.id.btn_getcount);
+        showitemname = findViewById(R.id.spinner_workcount_itemname);
         Intent intent = getIntent();
         if(intent != null){
             nameget = intent.getStringExtra("name");
@@ -65,17 +70,33 @@ public class Doc_workcount extends AppCompatActivity {
                         showendtime.setText(i+"-"+String.valueOf(i1+1)+"-"+String.valueOf(i2));
                         getendtime = showendtime.getText().toString();
                     }
-                },2023, 2, 2);
+                },2023, 1, 1);
                 enddate.show();
                 Log.d("Doc_workcount",showendtime.getText().toString());
+            }
+        });
+        showitemname.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                getitemname = Doc_workcount.this.getResources().getStringArray(R.array.item)[i];
+//                Log.d("Doc_workcount","查询索引i："+itemindex);
+
+                Log.d("Doc_workcount","查询项目名"+getitemname);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(Doc_workcount.this,"未选择项目",Toast.LENGTH_SHORT).show();
             }
         });
         getworkcount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                chargesum = 0;
                 Log.d("Doc_workcount","医生为"+nameget);
                 SQLiteDatabase db =dbHelper.getWritableDatabase();
-                Cursor cursor = db.query("Item",null,"starttime BETWEEN ? AND ? AND doc = ?",new String[]{getstarttime,getendtime,nameget},null,null,null);
+                Cursor cursor = db.query("Item",null,"starttime BETWEEN ? AND ? AND doc = ? AND itemname = ?",new String[]{getstarttime,getendtime,nameget,getitemname},null,null,null);
+                Log.d("Doc_workcount","cursor____"+cursor.moveToFirst());
                 if (cursor.moveToFirst()){
                     do {
                         charge = cursor.getInt(cursor.getColumnIndexOrThrow("chargenum"));
